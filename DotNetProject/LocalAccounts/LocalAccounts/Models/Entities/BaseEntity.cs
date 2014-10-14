4fd6ef3c03a659a5eb2ae706da5994a4c7e30780
Shohdi ElSHeemy <shohdi@gmail.com>
2014-10-15 01:25:00 +0200

    add repository interface

---
+++ DotNetProject/LocalAccounts/LocalAccounts/Models/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocalAccounts.Models.Entities
+{
+    public abstract partial class BaseEntity
+    {
+
+    }
+}
---
+++ DotNetProject/LocalAccounts/LocalAccounts/Models/Repo/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using LocalAccounts.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocalAccounts.Models.Repo
+{
+   
+        /// <summary>
+        /// Repository
+        /// </summary>
+        public partial interface IRepository<T> where T : BaseEntity
+        {
+            T GetById(object id);
+            bool Insert(T entity);
+            bool Update(T entity);
+            bool Delete(T entity);
+            IQueryable<T> Table { get; }
+
+        
+    }
+}
